447dee4060c793cdd6cadae5168f144b03909952
Guilherme Fernandes <gfengquimica@gmail.com>
2023-01-17 16:01:54 -0300

    feat(req03): criação da função para localizar próximo botão

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -28,7 +28,9 @@ def scrape_updates(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next_page = selector.css(".next::attr(href)").get()
+    return next_page
 
 
 # Requisito 4
