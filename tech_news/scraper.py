398011d0574782a1e0a23d2476a3e7d8184af1b7
Guilherme Fernandes <gfengquimica@gmail.com>
2023-01-17 15:19:09 -0300

    feat(req01): criação da função de fetch da página html

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,22 @@
-# Requisito 1 
+import requests
+from time import sleep
+
+
+# Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    try:
+        sleep(1)
+        response = requests.get(
+            url,
+            timeout=3,
+            headers={"user-agent": "Fake user-agent"},
+        )
+        response.raise_for_status()
+    except (requests.HTTPError, requests.ReadTimeout):
+        return None
+    else:
+        return response.text
 
 
 # Requisito 2
