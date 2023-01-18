9fd726bcd820caa91ea89e36bf31beec6fd5c975
Guilherme Fernandes <gfengquimica@gmail.com>
2023-01-18 11:19:42 -0300

    fix(req10): adiciona ordenação por número de comentários

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,10 +1,15 @@
 from tech_news.database import find_news
 from collections import Counter
+from operator import itemgetter
 
 
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
+    news = find_news()
+    alphabetic_news = sorted(news, key=itemgetter('title'))
+    popular_news = sorted(alphabetic_news, key=itemgetter('comments_count'))
+    return popular_news[:5]
 
 
 # Requisito 11
@@ -15,3 +20,6 @@ def top_5_categories():
     categories_count = Counter(sorted(categories)).most_common()
     categories_top_five = [category[0] for category in categories_count]
     return categories_top_five[:5]
+
+
+# print(top_5_news())
