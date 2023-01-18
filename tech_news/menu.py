a6d275f8022d275713f417721a2eeddb93e246ad
Guilherme Fernandes <gfengquimica@gmail.com>
2023-01-18 13:27:44 -0300

    feat(req12): implementa o menu para retorno das função esperada

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,22 @@
+array_options = [
+    'Selecione uma das opções a seguir:\n',
+    ' 0 - Popular o banco com notícias;\n',
+    ' 1 - Buscar notícias por título;\n',
+    ' 2 - Buscar notícias por data;\n',
+    ' 3 - Buscar notícias por tag;\n',
+    ' 4 - Buscar notícias por categoria;\n',
+    ' 5 - Listar top 5 notícias;\n',
+    ' 6 - Listar top 5 categorias;\n',
+    ' 7 - Sair.',
+]
+
+
 # Requisito 12
 def analyzer_menu():
     """Seu código deve vir aqui"""
+    options = "".join(array_options)
+    option = input(options)
+    return option
+
+
+# analyzer_menu()
