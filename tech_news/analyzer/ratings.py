9f76347652634fe9c83443026204b65184fdad49
Guilherme Fernandes <gfengquimica@gmail.com>
2023-01-18 10:14:36 -0300

    feat(req11): implementação da função para selecionar cinco categorias mais comuns

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,7 @@
+from tech_news.database import find_news
+from collections import Counter
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
@@ -6,3 +10,8 @@ def top_5_news():
 # Requisito 11
 def top_5_categories():
     """Seu código deve vir aqui"""
+    news = find_news()
+    categories = [new["category"] for new in news]
+    categories_count = Counter(categories).most_common()
+    categories_top_five = [category[0] for category in categories_count]
+    return categories_top_five[:5]
